92471a11b1e989fd2745399c9065166abedced9f
wangpengfei <wangpengfei@chelun.com>
2019-11-22 00:24:19 +0800

    3切片的操作-half

--- chapter_three/slices.go
+++ chapter_three/slices.go
@@ -37,4 +37,10 @@ func main() {
 	fmt.Println("s2=", s2)
 	fmt.Println(s1, len(s1), cap(s1))
 	fmt.Println(s2, len(s2), cap(s2))
+
+	s3 := append(s2, 10)
+	s4 := append(s3, 11)
+	s5 := append(s4, 12)
+	fmt.Println("s3,s4,s5", s3, s4, s5)
+	fmt.Println("arr=", arr)
 }
