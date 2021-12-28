b4e1379feb213e8db4d740464a6cc0216518266f
wangpengfei1 <wangpengfei1@chelun.com>
2021-12-28 18:53:07 +0800

    update-document

--- chapter_three/norepeating.go
+++ chapter_three/norepeating.go
@@ -2,25 +2,26 @@ package main
 
 import "fmt"
 
-func lengthOfNoRepeatingSubStr(s string) bool {
+func lengthOfNoRepeatingSubStr(s string) int {
 	lastOccurred := make(map[byte]int)
-	//start := 0
-	//maxLength := 0
-	_, ok := lastOccurred[0]
-	return ok
-	//for i, ch := range []byte(s) {
-	//	if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
-	//		start = lastI + 1
-	//	}
-	//	if i-start+1 > maxLength {
-	//		maxLength = i - start + 1
-	//	}
-	//	lastOccurred[ch] = i
-	//}
-	//return maxLength
+	start := 0
+	maxLength := 0
+	for i, ch := range []byte(s) {
+		//fmt.Printf("i=%d,ch=%c,lastOccurred[ch]=%d,start=%d,maxLength=%d\n", i, ch, lastOccurred[ch], start, maxLength)
+		lastI, ok := lastOccurred[ch]
+		//fmt.Printf("lastI=%d,ok=%t\n", lastI, ok)
+		if ok && lastI >= start {
+			start = lastI + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+	return maxLength
 }
 
 func main() {
-	test := "1111"
+	test := "abc"
 	fmt.Println(lengthOfNoRepeatingSubStr(test))
 }
